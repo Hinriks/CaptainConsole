1bb03d6fe821268b923f96c9a53b60872943f821
palmiv <palmividar17@gmail.com>
2020-05-04 19:47:20 +0000

    removed venv
    git commit -m removed

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\palmi\OneDrive\0 HR\Verklegt 2\CaptainConsole-project\captain\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
