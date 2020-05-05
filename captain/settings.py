fdac0a85a2eaec91876947421b0c217cfae3a1c8
palmiv <palmividar17@gmail.com>
2020-05-05 19:06:27 +0000

    Komid fullt af sidum til ad prufa

--- captain/settings.py
+++ captain/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'account.apps.AccountConfig',
+    'cart.apps.CartConfig',
+    'category.apps.CategoryConfig',
+    'product.apps.ProductConfig',
+
 ]
 
 MIDDLEWARE = [
@@ -119,3 +124,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static'),
+]
--- captain/urls.py
+++ captain/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #path('', include('captain.urls')),
+    path('cart/', include('cart.urls')),
+    path('account/', include('account.urls')),
+    path('product/', include('product.urls')),
+    path('category/', include('category.urls')),
 ]
