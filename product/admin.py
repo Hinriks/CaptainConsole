3df7a26637ffee9911fbf164f0b74900861c36b2
palmiv <palmividar17@gmail.com>
2020-05-07 19:14:29 +0000

    made superuser and way to add products

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Manufacturer, Product, ProductImage, Category
+
+admin.site.register(Manufacturer)
+admin.site.register(Product)
+admin.site.register(ProductImage)
+admin.site.register(Category)
--- product/models.py
+++ product/models.py
@@ -4,6 +4,8 @@ class Manufacturer(models.Model):
     name = models.CharField(max_length=255)
     logo = models.CharField(max_length=999, blank=True)
     year_of_start = models.DateTimeField()
+    def __str__(self):
+        return self.name
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
@@ -15,6 +17,8 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     price = models.IntegerField()
     manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
 
 class ProductImage(models.Model):
     image = models.CharField(max_length=999)
