2d9a0937a95deeb5c187076678f0bc938f5899be
palmiv <palmividar17@gmail.com>
2020-05-11 21:11:20 +0000

    shopping cart bby

--- cart/urls.py
+++ cart/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="cart-index"),
+    path('', views.view_cart, name="cart-index"),
     path('add_to_cart/<int:id>', views.add_to_cart, name="add_to_cart"),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,8 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, request
 from django.shortcuts import render, redirect, get_object_or_404
 from product.models import Product
+from user.models import User
+from cart.models import CartItem
 
 # Create your views here.
 
@@ -10,10 +12,72 @@ def index(request):
     return render(request, 'cart/index.html')
 
 
-def add_to_cart(request, id):
-    #Virkar ekki shit
-    product = {'product': get_object_or_404(Product, pk=id)}
-    request.session['cart'] = {}
-    request.session['cart'] = product
 
-    return redirect('product-details', id=id)
+
+def add_to_cart(request, id):
+    product_id = id
+    if request.user.is_authenticated:
+        username_from_request = request.user.username
+        user = User.objects.get(username=username_from_request)
+
+        if not user.cart:
+            new_cart = Cart()
+            new_cart.user = user
+            new_cart.save()
+
+        product = Product.objects.get(pk=product_id)
+        cart_item = CartItem()
+        cart_item.product = product
+        cart_item.cart = user.cart
+        cart_item.quantity = 1
+        cart_item.line_total = cart_item.product.price * cart_item.quantity
+        cart_item.save()
+            
+    return render(request, 'cart/index.html')
+
+def view_cart(request):
+    username_from_request = request.user.username
+    user = User.objects.get(username=username_from_request)
+    cartitems = user.cart.cart_item.all()
+    print(cartitems)
+    context = {'cartitems': cartitems}
+    print(context)
+    return render(request, 'cart/index.html', context)
+
+
+
+
+
+"""
+    username_from_request = request.user.username
+    user = User.objects.get(username=username_from_request)
+    cart = user.cart
+    context = {'cart': }
+    return render(request, 'product/index.html', context)
+ """
+            
+
+
+
+
+
+
+
+    #TODO ALLT HEHE
+"""
+    #Virkar ekki shit
+    if request.GET:
+        pass
+    if request.POST:
+        user = request.logged_in_user
+        if user.cart:
+            cart_item = CartItem()
+        else:
+            cart1 = Cart()
+            cart_item1 = CartItem()
+            cart1.user = user
+            cart_item1.cart = cart1
+            .
+            .
+            cart1.save()
+"""
