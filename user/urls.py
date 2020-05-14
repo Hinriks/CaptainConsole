8bb8fbd18863c484eb0ba19ad5ecf43733689356
palmiv <palmividar17@gmail.com>
2020-05-14 14:56:40 +0000

    gerdi login og logout og tengdi profile

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('profile', views.view_profile, name="profile"),
     path('profile/edit', views.edit_profile, name="edit profile"),
+    path('', include('django.contrib.auth.urls')),
 ]
