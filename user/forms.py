1eb2766a2a6538f539a1d8fb2798358d052a8ee5
Hinriks <hinrik97@hotmail.com>
2020-05-15 17:31:27 +0000

    edit profile tengt

--- user/forms.py
+++ user/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.forms import ModelForm, widgets
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
 User = get_user_model()
@@ -12,3 +13,16 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name','email', 'password1', 'password2',)
+
+class UserUpdateForm(ModelForm):
+    class Meta:
+        model = User
+        exclude = [ 'id', 'password', 'date_joined' ]
+        widgets = {
+            'first_name': widgets.TextInput(attrs={ 'class': 'form-control' }),
+            'last_name': widgets.TextInput(attrs={ 'class': 'form-control' }),
+            'email': widgets.TextInput(attrs={ 'class': 'form-control' }),
+            'phone_number': widgets.TextInput(attrs={ 'class': 'form-control' }),
+            'username': widgets.TextInput(attrs={ 'class': 'form-control' }),
+            'avatar_img': widgets.TextInput(attrs={ 'class': 'form-control' }),
+        }
--- user/urls.py
+++ user/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 urlpatterns = [
     path('profile', views.view_profile, name="profile"),
-    path('profile/edit', views.edit_profile, name="edit profile"),
+    path('profile/edit', views.edit_profile, name="edit_profile"),
+    path('profile/update', views.update_user, name="update_user"),
     path('signup', views.signup_view, name='signup'),
     path('', include('django.contrib.auth.urls')),
 ]
