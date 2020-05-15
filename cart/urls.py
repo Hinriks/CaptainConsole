c32f7eccd323d5dac9b0da80918e84fcef0e5d23
Hinriks <hinrik97@hotmail.com>
2020-05-15 21:21:00 +0000

    finished checkout

--- cart/urls.py
+++ cart/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.view_cart, name="cart-index"),
     path('add_to_cart/<int:id>', views.add_to_cart, name="add_to_cart"),
-    path('order/', views.order, name="order"),
+    path('order/', views.complete_order, name="order"),
     path('order/<int:id>', views.receipt, name="receipt"),
     path('delete_cartitem/<int:id>', views.delete_cartitem, name="delete_cartitem"),
     path('checkout/', views.view_checkout, name='checkout'),
